8a011567c26ea6513b516f2fe9e701c2c651e6e6
Abdullah <abdullahsh123@hotmail.com>
2021-08-06 06:28:59 +0500

    Splash Activity completed

---
+++ app/src/main/java/com/techsoldev/tictactoegame/SplashActivity.java
@@ -0,0 +1,108 @@
+package com.techsoldev.tictactoegame;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.ViewPropertyAnimatorCompat;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    public static final int STARTUP_DELAY = 300;
+    public static final int ANIM_ITEM_DURATION = 1000;
+    public static final int ITEM_DELAY = 300;
+
+    public int SCREEN_SIZE;
+    public int SET_TRANSLATE;
+    private boolean animationStarted = false;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        SCREEN_SIZE =getScreenResolution(this);
+
+        if(SCREEN_SIZE >1300)
+        {
+            SET_TRANSLATE = -500;
+        }
+        else if(SCREEN_SIZE <=1300)
+        {
+            SET_TRANSLATE = -250;
+        }
+
+    }
+
+
+    private int getScreenResolution(Context context)
+    {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+
+        Toast.makeText(SplashActivity.this , "{" + width + "," + height + "}", Toast.LENGTH_SHORT).show();
+
+        return height ;
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+
+        if (!hasFocus || animationStarted) {
+            return;
+        }
+
+        animate();
+
+        super.onWindowFocusChanged(hasFocus);
+    }
+
+    private void animate() {
+        ImageView logoImageView = (ImageView) findViewById(R.id.img_logo);
+        ViewGroup container = (ViewGroup) findViewById(R.id.container);
+
+        ViewCompat.animate(logoImageView)
+                .translationY(SET_TRANSLATE)
+                .setStartDelay(STARTUP_DELAY)
+                .setDuration(ANIM_ITEM_DURATION).setInterpolator(
+                new DecelerateInterpolator(1.2f)).start();
+
+        for (int i = 0; i < container.getChildCount(); i++) {
+            View v = container.getChildAt(i);
+            ViewPropertyAnimatorCompat viewAnimator;
+
+            if (!(v instanceof Button)) {
+                viewAnimator = ViewCompat.animate(v)
+                        .translationY(50).alpha(1)
+                        .setStartDelay((ITEM_DELAY * i) + 500)
+                        .setDuration(1000);
+            } else {
+                viewAnimator = ViewCompat.animate(v)
+                        .scaleY(1).scaleX(1)
+                        .setStartDelay((ITEM_DELAY * i) + 500)
+                        .setDuration(500);
+            }
+
+            viewAnimator.setInterpolator(new DecelerateInterpolator()).start();
+        }
+    }
+}
