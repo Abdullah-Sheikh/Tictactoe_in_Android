dff7654959c07f4b9fa5366dcb5cb7a05d3e0087
Abdullah <abdullahsh123@hotmail.com>
2021-08-13 04:56:12 +0500

    Choose Symbol Activity completed

--- app/src/main/java/com/techsoldev/tictactoegame/ChooseSymbolActivity.java
+++ app/src/main/java/com/techsoldev/tictactoegame/ChooseSymbolActivity.java
@@ -2,7 +2,10 @@ package com.techsoldev.tictactoegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -10,7 +13,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 
-public class ChooseSymbolActivity extends AppCompatActivity {
+public class ChooseSymbolActivity extends AppCompatActivity implements View.OnTouchListener {
 
 
     private ImageView BackBtn , CrossImg , CrossRadioImg , CircleImg , CircleRadioImg;
@@ -34,8 +37,77 @@ public class ChooseSymbolActivity extends AppCompatActivity {
         CircleRadioImg= (ImageView) findViewById(R.id.pick_side_circle_radio);
 
         ContinueBtn = (Button) findViewById(R.id.pick_side_continue_btn);
+
+        // CrossRadioImg.setOnTouchListener(this);
+         CrossRadioImg.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+
+                 CrossRadioImg.setImageResource(R.drawable.radio_button_checked);
+                 CircleRadioImg.setImageResource(R.drawable.radio_button_unchecked);
+                 CircleImg.setAlpha(0.3f);
+                 CrossImg.setAlpha(1.0f);
+                 //Intent intent = new Intent(.this,Ch.class);
+                // startActivity(intent);
+             }
+         });
+
+        // CircleRadioImg.setOnTouchListener(this);
+         CircleRadioImg.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+
+
+                 CircleRadioImg.setImageResource(R.drawable.radio_button_checked);
+                 CrossRadioImg.setImageResource(R.drawable.radio_button_unchecked);
+                 CrossImg.setAlpha(0.3f);
+                 CircleImg.setAlpha(1.0f);
+
+                 //Intent intent = new Intent(.this,Ch.class);
+                 // startActivity(intent);
+             }
+         });
+
+        BackBtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+
+
+
+                 onBackPressed();
+                 //Intent intent = new Intent(.this,Ch.class);
+                 // startActivity(intent);
+             }
+         });
+
+        ContinueBtn.setOnTouchListener(this);
+        ContinueBtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+
+
+
+                 //Intent intent = new Intent(.this,Ch.class);
+                 // startActivity(intent);
+             }
+         });
     }
 
 
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (v == ContinueBtn) {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                v.setAlpha(0.5f);
+            } else {
+                v.setAlpha(1f);
+            }
+        }
+        return false;
+    }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
 }
