8681387d67452a80bad7fe5eec874aa7236f75d6
Abdullah <abdullahsh123@hotmail.com>
2021-08-13 03:57:13 +0500

    Choose Symbol Activity components binding complete

--- app/src/main/java/com/techsoldev/tictactoegame/ChooseSymbolActivity.java
+++ app/src/main/java/com/techsoldev/tictactoegame/ChooseSymbolActivity.java
@@ -6,11 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 
 public class ChooseSymbolActivity extends AppCompatActivity {
 
-    @Override
+
+    private ImageView BackBtn , CrossImg , CrossRadioImg , CircleImg , CircleRadioImg;
+    private Button ContinueBtn;
+
+
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -19,6 +26,14 @@ public class ChooseSymbolActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_choose_symbol);
+
+        BackBtn= (ImageView) findViewById(R.id.pick_side_back_btn);
+        CrossImg= (ImageView) findViewById(R.id.pick_side_cross_img);
+        CircleImg= (ImageView) findViewById(R.id.pick_side_circle_img);
+        CrossRadioImg= (ImageView) findViewById(R.id.pick_side_cross_radio);
+        CircleRadioImg= (ImageView) findViewById(R.id.pick_side_circle_radio);
+
+        ContinueBtn = (Button) findViewById(R.id.pick_side_continue_btn);
     }
 
 
