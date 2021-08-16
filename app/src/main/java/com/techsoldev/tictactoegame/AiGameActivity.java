b7478982782577c529efa782ab12e4eb868f6c96
Abdullah <abdullahsh123@hotmail.com>
2021-08-17 02:13:45 +0500

    AI Game Activity created

---
+++ app/src/main/java/com/techsoldev/tictactoegame/AiGameActivity.java
@@ -0,0 +1,14 @@
+package com.techsoldev.tictactoegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AiGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ai_game);
+    }
+}
