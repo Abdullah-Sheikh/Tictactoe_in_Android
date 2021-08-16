fc4717f659a2e200a57975ed5b769bc03c7d7996
Abdullah <abdullahsh123@hotmail.com>
2021-08-17 01:59:31 +0500

    AI choose symbol Activity created

---
+++ app/src/main/java/com/techsoldev/tictactoegame/AiChooseSymbolActivity.java
@@ -0,0 +1,14 @@
+package com.techsoldev.tictactoegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AiChooseSymbolActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ai_choose_symbol);
+    }
+}
